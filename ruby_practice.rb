dc4f73b58501155daea44b36b83ea8843b7a955b
jeffspatafora <jeffspatafora@gmail.com>
2022-12-08 12:27:18 -0800

    adds practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -29,12 +29,21 @@ end
 
 shorter_than_four(["a", "man", "a", "plan", "a", "canal", "panama"])
 
+
+
+def shorter_than_four(strings)
+  less_than_four = []
+  p strings.select { |string| (string.length < 4) }
+end
+
+shorter_than_four(["a", "man", "a", "plan", "a", "canal", "panama"])
+
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-def names_less_than(people)
-  names_less_than = people.select do |person|
-    person[:name].length < 6
+def names_less_than(things)
+  names_less_than = things.select do |thing|
+    thing[:name].length < 6
   end
   p names_less_than
 end
@@ -47,11 +56,37 @@ names_less_than([{name: "chair", price: 100}, {name: "pencil", price: 1}, {name:
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
+def less_than_ten(numbers)
+  p numbers.select {|number| number < 10}
+end 
+
+less_than_ten([8, 23, 0, 44, 1980, 3])
+
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+def first_not_b(strings)
+  p strings.select {|string| string[0].downcase != 'b'}
+end
+
+first_not_b(["big", "little", "good", "bad"])
+
+
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+def prices_less_than_ten(items)
+  p items.select {|item| item[:price] < 10}
+end
+
+
+prices_less_than_ten([{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}])
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+def only_odd(numbers)
+  p numbers.select {|number| number.odd?}
+end
+
+only_odd([2, 4, 5, 1, 8, 9, 7])
