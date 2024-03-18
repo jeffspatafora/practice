46326d82a4f4aea22a07545f581eb60bef10c6b6
jeffspatafora <jeffspatafora@gmail.com>
2024-03-18 16:33:09 -0700

    adds work to reset git token

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -537,21 +537,21 @@
 # Input: [5, 10, 8, 3]
 # Output: 10
 
-def greatest_number(numbers)
-  current_greatest_number = numbers[0]
-  i = 0
-  while i < numbers.length
-    if numbers[i] > current_greatest_number
-      current_greatest_number = numbers[i]
-      i += 1
-    else
-      i += 1
-    end
-  end
-  p current_greatest_number
-end
+# def greatest_number(numbers)
+#   current_greatest_number = numbers[0]
+#   i = 0
+#   while i < numbers.length
+#     if numbers[i] > current_greatest_number
+#       current_greatest_number = numbers[i]
+#       i += 1
+#     else
+#       i += 1
+#     end
+#   end
+#   p current_greatest_number
+# end
 
-greatest_number([5, 10, 22, 3, 11, 44, 5, 0, -1])
+# greatest_number([5, 10, 22, 3, 11, 44, 5, 0, -1])
 
 
 
@@ -579,3 +579,32 @@ greatest_number([5, 10, 22, 3, 11, 44, 5, 0, -1])
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
 
+##################################
+
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+num = 9
+
+if num == 10
+  p 0
+else
+  p - 1
+end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
