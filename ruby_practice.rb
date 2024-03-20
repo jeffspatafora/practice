86fc8c441fb437032dfcfab1daf723bb2e53cf11
jeffspatafora <jeffspatafora@gmail.com>
2024-03-20 01:28:12 -0700

    adds deliberate practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -588,14 +588,42 @@ num = 9
 if num == 10
   p 0
 else
-  p - 1
+  p -1
 end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
+num = 7
+
+if num < 10
+  p -1
+elsif num > 10
+  p 1
+elsif num == 10
+  p 0
+end 
+
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
+num_1 = 2
+num_2 = 11
+
+if num_1 < 10 && num_2 < 10
+  p 1
+else
+  p 0
+end
+
+
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+num = 10
+
+if num > 9000
+  p 1
+else
+  p -1
+end
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
