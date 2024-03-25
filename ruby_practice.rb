e1e582355b512af39f50bd8719505a6b3b42f274
jeffspatafora <jeffspatafora@gmail.com>
2024-03-25 12:09:10 -0700

    adds basics pratice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -772,11 +772,34 @@ end
 
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
+person = { "first_name" => 'petey', last_name: 'catafora', email: 'petey@email.com'}
+
+p person['first_name']
+p person[:last_name]
+p person[:email]
+
 
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
+people = [
+  { first_name: 'jib', last_name: 'son', email: 'jibson@email.com' },
+  { first_name: 'jon', last_name: 'sob', email: 'jonsob@email.com' },
+  { first_name: 'jib', last_name: 'son', email: 'jibson@email.com' }
+]
+
+
+p people[0]
+
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+menu = { hotdog: 70, fries: '2K', drink: '$60'}
+
+p menu
+
+menu[:pizza] = 100
+
+p menu
 
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
@@ -787,6 +810,15 @@ end
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+laptops = [
+  { brand: 'honda', model: 'spacebook' },
+  { brand: 'john_dear', model: 'oldbook 2' },
+  { brand: 'dear_john', model: 'laptop 3' }
+]
+
+p laptops[1][:model]
+
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
