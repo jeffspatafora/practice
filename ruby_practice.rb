43aabdc2d477a1ec1e3158903f3684604731fbe4
jeffspatafora <jeffspatafora@gmail.com>
2025-06-02 00:13:49 -0500

    adds deliberate practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -262,3 +262,73 @@ end
 
 # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
 
+
+
+
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+# 
+
+p "please enter a word:"
+
+word = gets.chomp
+
+p word
+
+p word.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+# 
+
+p "enter a number:"
+
+number = gets.chomp
+
+if number.to_i > 100
+  p "that's a big number"
+else
+  p "the number is less than 100"
+end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
+
+
+
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
