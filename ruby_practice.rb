e5b486d4e45fd0e85cb024059c0a5b8613c29b18
jeffspatafora <jeffspatafora@gmail.com>
2026-08-08 15:28:29 -0500

    adds practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,69 +1,47 @@
-# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-number = 7
+first_name = "jib"
+last_name = "smith"
 
-p number
+p first_name
+p last_name
 
-if number == 10
-  p 0
-else
-  p -1
+p first_name + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+
+first_name = "jif"
+last_name = "don"
+
+p first_name
+p last_name
+
+p "#{first_name} #{last_name}"
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+p "enter a word:"
+word = gets.chomp
+
+p word
+
+if word == "marco"
+  p "polo"
 end
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-p "input a number"
-number = gets.chomp
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
-number = number.to_i
- 
-p number
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
-if number < 10
-  p -1
-elsif number > 10
-  p 1
-elsif number == 10
-  p 0
-end
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
 
-
-# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-
-# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-
-# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-
-# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-
-# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-
-# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-
-# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-
-# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
-
-
-p "type a number:"
-number = gets.chomp
-
-number = number.to_i
-
-p number
-
-if number > 80
-  p 5
-elsif number > 60
-  p 4
-elsif number > 40
-  p 3
-elsif number > 20
-  p 2
-else
-  p 1
-end
-
-
-# SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
+# SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
